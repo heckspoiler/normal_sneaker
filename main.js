6b4273f6cab20e984e420c278e2e4beaa7a22e3c
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2024-05-20 16:02:03 +0200

    adding scene three.js

--- main.js
+++ main.js
@@ -1,6 +1,21 @@
 import { animate, inView } from 'motion';
-
+import {
+  Scene,
+  PerspectiveCamera,
+  WebGLRenderer,
+  MeshLambertMaterial,
+  Mesh,
+  TorusKnotGeometry,
+  AmbientLight,
+  DirectionalLight,
+} from 'three';
 import './style.css';
+
+// tag selection
+
+const sneakerTag = document.querySelector('.sneaker');
+
+// motion one
 
 animate(
   'header',
@@ -33,16 +48,39 @@ inView('section.content', (info) => {
 
 // three.js
 
-import * as THREE from 'three';
-
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(
+const scene = new Scene();
+const camera = new PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
   0.1,
   1000
 );
 
-const renderer = new THREE.WebGLRenderer();
+const renderer = new WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+renderer.setClearColor(0x00000, 0);
+sneakerTag.appendChild(renderer.domElement);
+
+// Lighting
+
+const ambience = new AmbientLight(0xfffffff);
+const keyLight = new DirectionalLight(0xffffff, 1);
+
+keyLight.position.set(-1, 1, 3);
+
+scene.add(ambience, keyLight);
+
+const geometry = new TorusKnotGeometry(1, 0.25, 100, 10);
+const material = new MeshLambertMaterial({ color: 0xffff00, wireframe: false });
+const cube = new Mesh(geometry, material);
+scene.add(cube);
+
+camera.position.z = 5;
+
+const render = () => {
+  cube.rotation.x += 0.005;
+  cube.rotation.y += 0.005;
+  requestAnimationFrame(render);
+  renderer.render(scene, camera);
+};
+render();
